31a2c741a89928e2f518d7c2669f5315743e79be
Olof Enström <oe222fh@student.lnu.se>
2022-05-13 14:20:17 +0200

    Cleanup exercise2 code and add picture

--- exercise2.py
+++ exercise2.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
 from mnist_load import mnist
 from threading import Thread
 import time
@@ -28,18 +28,20 @@ def one_vs_all(clf, X_train, y_train, X_test):
 
     return labels[prob_table.argmax(1)]
 
+
 def main():
     """Main function to run when script is run."""
     train_images, train_labels, X_test, y_test = mnist("A3_data")
 
-    train_size = 10000
+    train_size = 20000
     X_train = train_images[:train_size]
     y_train = train_labels[:train_size]
-    val_size = 1000
+    val_size = 4000
     X_val = train_images[train_size:train_size + val_size]
     y_val = train_labels[train_size:train_size + val_size]
     
 
+    print("Searching for hyperparameters...")
     params = {
         "C": np.linspace(1, 10, 10),
         "gamma": np.linspace(0.001, 0.1, 10)
@@ -49,16 +51,20 @@ def main():
     best_C, best_gamma = best_rbf.best_params_["C"], best_rbf.best_params_["gamma"]
     print(f"Best params: C={best_C}, gamma={best_gamma}")
 
-    rbf = SVC(kernel="rbf", C=best_C, gamma=best_gamma, random_state=42)
+    print("Fitting training data...")
+    rbf = SVC(kernel="rbf", C=best_C, gamma=best_gamma)
     rbf.fit(X_train, y_train)
-    print("OvO Train accuracy:", rbf.score(X_train, y_train) * 100, "%")
-    print("OvO Test accuracy:", rbf.score(X_test, y_test) * 100, "%")
+    print("Calculating scores...")
+    print("One-vs-One Train accuracy:", rbf.score(X_train, y_train) * 100, "%")
+    print("One-vs-One Test accuracy:", rbf.score(X_test, y_test) * 100, "%")
 
+    print("-------------------------------")
+    print("Performing custom One-vs-All...")
     start_time = time.time()
-    clf = SVC(kernel="rbf", C=best_C, gamma=best_gamma, random_state=42)
+    clf = SVC(kernel="rbf", C=best_C, gamma=best_gamma)
     y_pred = one_vs_all(clf, X_train, y_train, X_test)
 
-    print("OvR Test accuracy:", accuracy_score(y_test, y_pred) * 100, "%")
+    print("One-vs-All Test accuracy:", accuracy_score(y_test, y_pred) * 100, "%")
     print("Time:", time.time() - start_time, "seconds")
 
     labels = np.unique(y_train)
@@ -67,16 +73,8 @@ def main():
     ax1.title.set_text("Built in One-vs-One")
     ax2 = plt.subplot(1, 2, 2)
     ax2.title.set_text("Custom One-vs-All")
-    ConfusionMatrixDisplay.from_estimator(rbf, X_test, y_test, labels=labels, ax=ax1)
-    ConfusionMatrixDisplay.from_predictions(y_test, y_pred, labels=labels, ax=ax2)
-
-    # from sklearn.multiclass import OneVsOneClassifier
-    # start_time = time.time()
-    # clf = OneVsOneClassifier(SVC(kernel="rbf", C=best_C, gamma=best_gamma, random_state=42))
-    # clf.fit(X_train, y_train)
-    # Y_pred = clf.predict(X_test)
-    # print("Built-in OvO Test accuracy:", accuracy_score(y_test, Y_pred) * 100, "%")
-    # print("Time:", time.time() - start_time, "seconds")
+    ConfusionMatrixDisplay.from_estimator(rbf, X_test, y_test, labels=labels, ax=ax1, colorbar=False)
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred, labels=labels, ax=ax2, colorbar=False)
 
     plt.show()
 
